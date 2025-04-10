52e76ce2520c6e40343c30e0aa79690a5231c588
RamReddymanda <ramreddymandha87@gmail.com>
2025-04-10 12:37:59 +0530

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function toggleMenu(){
+    const menu=document.querySelector(".menu-links");
+    const icon=document.querySelector(".hamburger-icon");
+    icon.classList.toggle("open");
+    menu.classList.toggle("open");
+   
+
+
+}
